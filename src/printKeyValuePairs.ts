00d9147fd0f2952a68e4538115c46beecfdc9601
niamhbrockbank <niamh.brockbank@gmail.com>
2022-09-30 13:51:37 +0100

    Add printKeyValuePairs

---
+++ src/printKeyValuePairs.ts
@@ -0,0 +1,26 @@
+/*
+printAllKeyValuePairs
+  - params : object with string keys and  string values
+  - return void
+*/
+
+interface InputObject {
+    [name : string] : string
+  }
+  
+  function printAllKeyValuePairs(obj : InputObject) : void{
+    for (let pair in obj){
+      console.log(pair + ' ' + obj[pair])
+    }
+  }
+  
+  //Tests
+  const characters = {
+    Bruce: "Hulk",
+    Peter: "Spiderman",
+    Ororo: "Storm"
+  };
+  
+  printAllKeyValuePairs(characters)
+  
+  console.log(Object.entries(characters))
