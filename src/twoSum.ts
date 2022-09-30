f1d269700474f5320eb14231223f0233c5047419
niamhbrockbank <niamh.brockbank@gmail.com>
2022-09-30 09:50:06 +0100

    Add basic approach to twoSum

--- src/twoSum.ts
+++ src/twoSum.ts
@@ -1,3 +1,11 @@
-export default function twoSum(): number[]{
-    return [42]
-}
+export default function twoSum(nums: number[], target: number): number[] {
+    for (let i = 0; i < nums.length - 1; i++){
+        for (let j = 0; j < nums.length; j++){
+            if (nums[i] + nums[j] === target){
+                return [i, j]
+            }
+        }
+    }
+
+    return [404, 404]
+};
