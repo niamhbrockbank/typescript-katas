fef5f800433fdedf1cd2698bd8782aad67e44266
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-04 10:06:10 +0000

    Add new katas 4th Nov

---
+++ src/complete/noughtsAndCrosses/noughtsAndCrosses.ts
@@ -0,0 +1,19 @@
+export default function noughtsAndCrosses(board : number[][]): number{
+
+    let rows = board;
+    let cols = board.map((cell, i) => [board[0][i], board[1][i], board[2][i]])
+    let diags = [[board[0][0], board[1][1], board[2][2]], [board[0][2], board[1][1], board[2][0]]]
+    const allSolutions = rows.concat(cols, diags)
+
+    let incomplete = false
+    for (const sol of allSolutions){
+        const sum = sol.reduce((a, b) => a + b)
+        if ((sum % 3 === 0) && (sol.indexOf(0) === -1)){
+            return sol[0]
+        } else if (sol.indexOf(0) !== -1) {
+            incomplete = true
+        }
+    }
+
+    return incomplete ? -1 : 0
+}
---
+++ src/inProgress/moleculesToAtoms/moleculesToAtoms.ts
@@ -0,0 +1,26 @@
+interface AtomRegister {
+    [atom : string] : number
+}
+
+export default function moleculesToAtoms(formula : string): AtomRegister{
+    let outputElements : AtomRegister = {}
+    const formulaLength = formula.length
+    
+    for (let i = 0; i < formulaLength; i++){
+        const char = formula[i]
+
+        if (char.match(/[A-Z]/)){
+            outputElements[char] = 1
+
+            if (i < formulaLength - 1){
+                const nextChar = formula[i + 1]
+                if (nextChar.match(/[1-9]/)){
+                    outputElements[char] *= parseInt(nextChar)
+                }
+            }
+        }
+
+    }
+
+    return outputElements
+}
