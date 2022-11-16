e0ab459eb18f1e31bc49d87a1964e2a37e0155c9
niamhbrockbank <niamh.brockbank@gmail.com>
2022-11-16 11:05:01 +0000

    Add scramble kata

---
+++ src/scramble/README.md
@@ -0,0 +1,5 @@
+# Scramble
+This kata function passes all the tests in the jest file. However, it is too slow an algorithm for longer test cases.
+
+## Goal
+[*] Re-attempt this kata with performance in mind
---
+++ src/scramble/scramble.ts
@@ -0,0 +1,18 @@
+//TODO: performance should be considered
+
+export default function scramble(str1 : string, str2 : string):boolean{
+    let copyStr1 = str1
+    const str2Length = str2.length
+
+    for (let i = 0; i < str2Length; i++){
+        const letter = str2[i]
+
+        if (!copyStr1.includes(letter)){
+            return false
+        } else {
+            const scrambledIndex = copyStr1.indexOf(letter)
+            copyStr1 = `${copyStr1.slice(0,scrambledIndex)}${copyStr1.slice(scrambledIndex + 1)}`
+        }
+    }
+    return true
+}
