cd5df505e99e4af7e11c0833e28157e880538d48
niamhbrockbank <niamh.brockbank@gmail.com>
2023-01-17 11:00:37 +0000

    Format with prettier

--- src/complete/trailingZeros/README.md
+++ src/complete/trailingZeros/README.md
@@ -1,14 +1,16 @@
 # Trailing Zeros
+
 **5kyu**
 Taken from codewars, [see here for full instructions](https://www.codewars.com/kata/52f787eb172a8b4ae1000a34).
 
 **Brief:**
 Write a function that returns the number of trailing zeros for a given number's factorial.
 
-**Tip:** 
+**Tip:**
 Factorials become very large, quickly (`1000!` has 2568 digits) so focus on making it an efficient algorithm.
 
 ## Examples
+
 ```
 zeros(6) = 1
 # 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
@@ -17,4 +19,4 @@ zeros(12) = 2
 # 12! = 479001600 --> 2 trailing zeros
 ```
 
-*Hint: See equation 3 [here](https://mathworld.wolfram.com/Factorial.html)*
+_Hint: See equation 3 [here](https://mathworld.wolfram.com/Factorial.html)_
--- src/inProgress/validateSudoku/validateSudoku.ts
+++ src/inProgress/validateSudoku/validateSudoku.ts
@@ -1,65 +1,65 @@
-type Board = number[][]
+type Board = number[][];
 
-export default function validSolution(board : Board): boolean{
-    let columns : number[][] = [...Array(9)];
-    let squares : number[][] = [...Array(9)]
+export default function validSolution(board: Board): boolean {
+  let columns: number[][] = [...Array(9)];
+  let squares: number[][] = [...Array(9)];
 
-    for (let int in board){
-        const i = parseInt(int)
-        const row = board[i]
+  for (let int in board) {
+    const i = parseInt(int);
+    const row = board[i];
 
-        //return false if there any zeros on the board
-        if (row.includes(0)){
-            return false
-        }
-
-        //return false if number is repeated within the row
-        if (!numbersAreUnique(row)){
-            console.log(`numbers aren't unique`)
-            return false
-        }
-
-        //Add numbers to column arrays
-        if (i === 0){
-            columns = row.map(num => [num])
-            squares = [row.slice(0,3), row.slice(3, 6), row.slice(6, 9)]
-        } else {
-            row.map((num, i) => columns[i] = [...columns[i], num])
-            const yPosSquare = Math.floor(i / 3)
-            //TODO: Find x position of square
-            
-        }
-        
+    //return false if there any zeros on the board
+    if (row.includes(0)) {
+      return false;
     }
 
-    //return false if number is repeated within column
-    for (let col of columns){
-        if (!numbersAreUnique(col)){
-            return false
-        }
+    //return false if number is repeated within the row
+    if (!numbersAreUnique(row)) {
+      console.log(`numbers aren't unique`);
+      return false;
     }
 
-    //return false if number is repeated within grid square
-    for (let sqr of squares){
-        if (!numbersAreUnique(sqr)){
-            return false
-        }
+    //Add numbers to column arrays
+    if (i === 0) {
+      columns = row.map((num) => [num]);
+      squares = [row.slice(0, 3), row.slice(3, 6), row.slice(6, 9)];
+    } else {
+      row.map((num, i) => (columns[i] = [...columns[i], num]));
+      const yPosSquare = Math.floor(i / 3);
+      //TODO: Find x position of square
     }
-
-    return true
   }
 
-  //TODO: Write this function
-  function numbersAreUnique(arr : number[]): boolean{
-    return true
+  //return false if number is repeated within column
+  for (let col of columns) {
+    if (!numbersAreUnique(col)) {
+      return false;
+    }
   }
 
-validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
-    [6, 7, 2, 1, 9, 5, 3, 4, 8],
-    [1, 9, 8, 3, 4, 2, 5, 6, 7],
-    [8, 5, 9, 7, 6, 1, 4, 2, 3],
-    [4, 2, 6, 8, 5, 3, 7, 9, 1],
-    [7, 1, 3, 9, 2, 4, 8, 5, 6],
-    [9, 6, 1, 5, 3, 7, 2, 8, 4],
-    [2, 8, 7, 4, 1, 9, 6, 3, 5],
-    [3, 4, 5, 2, 8, 6, 1, 7, 9]])
+  //return false if number is repeated within grid square
+  for (let sqr of squares) {
+    if (!numbersAreUnique(sqr)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+//TODO: Write this function
+function numbersAreUnique(arr: number[]): boolean {
+  return true;
+}
+
+validSolution([
+  [5, 3, 4, 6, 7, 8, 9, 1, 2],
+  [6, 7, 2, 1, 9, 5, 3, 4, 8],
+  [1, 9, 8, 3, 4, 2, 5, 6, 7],
+  [8, 5, 9, 7, 6, 1, 4, 2, 3],
+  [4, 2, 6, 8, 5, 3, 7, 9, 1],
+  [7, 1, 3, 9, 2, 4, 8, 5, 6],
+  [9, 6, 1, 5, 3, 7, 2, 8, 4],
+  [2, 8, 7, 4, 1, 9, 6, 3, 5],
+  [3, 4, 5, 2, 8, 6, 1, 7, 9],
+]);
